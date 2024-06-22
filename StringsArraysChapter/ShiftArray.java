ac6a4615080a41fc5f671d53b5dbab4d212b49ec
Gael Estrera <gaelthegoodboy@gmail.com>
2024-06-23 00:55:42 +0800

    Chapter 3: Rotate Arrays Done

--- StringsArraysChapter/ShiftArray.java
+++ StringsArraysChapter/ShiftArray.java
@@ -1,32 +1,54 @@
 package StringsArraysChapter;
-import java.lang.reflect.Array;
 import java.util.*;
 
 public class ShiftArray {
 
     //Without making a new array
-    public static int[] shift(int[] a, int rotations)
+    public static int[] shift(int[] a, int rotations, String LorR)
     {
 
-        //Shift based on rotations
-        for (int i = 0; i < rotations; i++)
+        if (LorR.equals("R"))
         {
-            //Put last element to the first element position
-            int lastTemp = a[a.length - 1];
-            int firstTemp = a[0];
-            a[0] = lastTemp;
-
-            //At this point, firstTempt is in suspense
-            //We first adjust the elements from the last to the 2nd to the first
-            for (int j = a.length - 1; j > 1; j--)
+            //Shift based on rotations
+            for (int i = 0; i < rotations; i++)
             {
-                a[j] = a[j - 1];
+                //Put last element to the first element position
+                int lastTemp = a[a.length - 1];
+                int firstTemp = a[0];
+                a[0] = lastTemp;
+
+                //At this point, firstTempt is in suspense
+                //We first adjust the elements from the last to the 2nd to the first
+                for (int j = a.length - 1; j > 1; j--)
+                {
+                    a[j] = a[j - 1];
+                }
+
+                //We then append the first element ot the second position
+                a[1] = firstTemp;
+
             }
+        } 
+        else  
+        {
+            for (int i = 0; i < rotations; i++)
+            {
+                //Put first element to the last position
+                int firstTemp = a[0];
+                int lastTemp = a[a.length - 1];
+                a[a.length - 1] = firstTemp;
 
-            //We then append the first element ot the second position
-            a[1] = firstTemp;
+                //At this point, lastTemp is in suspense
+                //Move elements in the middle from right to left
+                for (int j = 0; j < a.length - 1; j++)
+                {
+                    a[j] = a[j + 1];
+                }
 
+                a[a.length - 2] = lastTemp;
+            }
         }
+        
         
         System.out.println(Arrays.stream(a).boxed().toList());
         return a;
@@ -35,6 +57,6 @@ public class ShiftArray {
     public static void main (String args[])
     {
         int[] test = {1, 2, 3, 4, 5};
-        System.out.println(shift(test, 4));
+        System.out.println(shift(test, 2, "L"));
     }
 }
