118f330efc08a8ecde310cf654d390d68988a9f8
Gael Estrera <gaelthegoodboy@gmail.com>
2024-07-04 19:12:54 +0800

    Merge Sort: New Array from 2 arrays

--- SortAlgorithms/MergeSort.java
+++ SortAlgorithms/MergeSort.java
@@ -6,13 +6,13 @@ public class MergeSort { //https://www.geeksforgeeks.org/merge-sort/
 
 	public static int[] mergeArray(int[]a1, int[]a2)
 	{
-		int max = Math.max(a1.length, a2.length);
-		int[] c = new int[max];
+	  int max = Math.max(a1.length, a2.length);
+	  int[] c = new int[max];
 	}
 
 	public static void main (String args[])
 	{
-		int[]a = {1, 5, 200, 32};
-		int[]b = {2,21, 2, 9};
+	  int[]a = {1, 5, 200, 32};
+	  int[]b = {2,21, 2, 9};
 	}
 }
